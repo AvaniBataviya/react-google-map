8b208c88aaef839548ef96004534bb3fd1ffccc2
Avani Bataviya <avanibataviya2610@gmail.com>
2020-09-27 19:59:49 +0530

    Added google map

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import Header from "./component/Header";
 import Map from "./component/Map";
 
 function App() {
-  return (
-    <main>
-      <Header/>
-      <h1 className="text-center header mt-5">Hello Developers...!</h1>
-      <Map/>
-    </main>
-  );
+	return (
+		<main>
+			<Header/>
+			<Map/>
+		</main>
+	);
 }
 
 export default App;
--- src/component/Map.js
+++ src/component/Map.js
@@ -1,15 +1,32 @@
 import React from "react";
+import {compose, withProps} from "recompose";
+import {withScriptjs, withGoogleMap, GoogleMap, Marker} from "react-google-maps";
 
-const Map = () => {
+const MyMapComponent = compose(
+	withProps({
+		loadingElement: <div style={{height: `100%`}}/>,
+		containerElement: <div style={{height: `500px`}}/>,
+		mapElement: <div style={{height: `100%`}}/>,
+	}),
+	withScriptjs,
+	withGoogleMap,
+)(props => (
+	<GoogleMap
+		defaultZoom={8}
+		defaultCenter={{lat: props.lat, lng: props.lng}}
+	>
+		<Marker position={{lat: props.lat, lng: props.lng}}/>
+	</GoogleMap>
+));
+
+export default () => {
+	const mapApiKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+	const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=${mapApiKey}`;
 	return (
-		<div className="container mt-5">
-			<div className="row">
-				<div className="col-12">
-					Map goes here...!
-				</div>
-			</div>
-		</div>
-	)
+		<MyMapComponent
+			googleMapURL={googleMapURL}
+			lat={23.0225}
+			lng={72.5714}
+		/>
+	);
 };
-
-export default Map;
